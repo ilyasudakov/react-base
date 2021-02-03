41bab9d53407f9ef4ec04ffc67c7b40c2a9f0437
ilyasudakov <ilyasudakov@inbox.ru>
2021-02-03 10:49:13 +0300

    update App test case

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App.jsx';
+import { App } from './App.jsx';
 
 describe('App component', () => {
   test('renders', () => {
