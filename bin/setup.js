39e50bd3ccbcc4e1763b3e054afd9d7cc18df4a3
ilyasudakov <ilyasudakov@inbox.ru>
2021-02-03 09:33:16 +0300

    add setup script

---
+++ bin/setup.js
@@ -0,0 +1,226 @@
+#!/usr/bin/env node
+
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const readline = require("readline");
+const compareVersions = require("compare-versions");
+// const shell = require("shelljs");
+// const chalk = require("chalk");
+
+const npmConfig = require("./helpers/get_npm_config.js");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+process.stdout.write("\n");
+let interval = -1;
+
+function deleteFileInCurrentDir(file) {
+  return new Promise((resolve, reject) => {
+    fs.unlink(path.join(__dirname, file), (err) => reject(new Error(err)));
+    resolve();
+  });
+}
+
+function removeGitRepository() {
+  return new Promise((resolve, reject) => {
+    exec("rm -rf .git/", (error) => {
+      if (error) reject(new Error(error));
+
+      resolve();
+    });
+  });
+}
+
+function askUserIfWeShouldRemoveRepo() {
+  return new Promise((resolve) => {
+    process.stdout.write(
+      "\nDo you want to start with a new repository? [Y/n] "
+    );
+    process.stdin.resume();
+    process.stdin.on("data", (pData) => {
+      const answer = pData.toString().trim().toLowerCase() || "y";
+
+      /* eslint-disable-next-line no-unused-expressions */
+      answer === "y" ? resolve(true) : resolve(false);
+    });
+  });
+}
+
+async function cleanCurrentRepository() {
+  const hasGitRepo = await hasGitRepository().catch((reason) =>
+    reportError(reason)
+  );
+
+  // We are not under Git version control. So, do nothing
+  if (hasGitRepo === false) {
+    return false;
+  }
+
+  const answer = await askUserIfWeShouldRemoveRepo();
+
+  if (answer === true) {
+    process.stdout.write("Removing current repository");
+    await removeGitRepository().catch((reason) => reportError(reason));
+    // addCheckMark();
+  }
+
+  return answer;
+}
+
+function installPackages() {
+  return new Promise((resolve, reject) => {
+    process.stdout.write(
+      "\nInstalling dependencies... (This might take a while)"
+    );
+
+    setTimeout(() => {
+      readline.cursorTo(process.stdout, 0);
+      // interval = animateProgress("Installing dependencies");
+    }, 500);
+
+    exec("npm install", (err) => {
+      if (err) {
+        reject(new Error(err));
+      }
+
+      clearInterval(interval);
+      //   addCheckMark();
+      resolve("Packages installed");
+    });
+  });
+}
+
+function initGitRepository() {
+  return new Promise((resolve, reject) => {
+    exec("git init", (err, stdout) => {
+      if (err) {
+        reject(new Error(err));
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+function addToGitRepository() {
+  return new Promise((resolve, reject) => {
+    exec("git add .", (err, stdout) => {
+      if (err) {
+        reject(new Error(err));
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+function commitToGitRepository() {
+  return new Promise((resolve, reject) => {
+    exec('git commit -m "Initial commit"', (err, stdout) => {
+      if (err) {
+        reject(new Error(err));
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+function checkNodeVersion(minimalNodeVersion) {
+  return new Promise((resolve, reject) => {
+    exec("node --version", (err, stdout) => {
+      const nodeVersion = stdout.trim();
+      if (err) {
+        reject(new Error(err));
+      } else if (compareVersions(nodeVersion, minimalNodeVersion) === -1) {
+        reject(
+          new Error(
+            `You need Node.js v${minimalNodeVersion} or above but you have v${nodeVersion}`
+          )
+        );
+      }
+
+      resolve("Node version OK");
+    });
+  });
+}
+
+function checkNpmVersion(minimalNpmVersion) {
+  return new Promise((resolve, reject) => {
+    exec("npm --version", (err, stdout) => {
+      const npmVersion = stdout.trim();
+      if (err) {
+        reject(new Error(err));
+      } else if (compareVersions(npmVersion, minimalNpmVersion) === -1) {
+        reject(
+          new Error(
+            `You need NPM v${minimalNpmVersion} or above but you have v${npmVersion}`
+          )
+        );
+      }
+
+      resolve("NPM version OK");
+    });
+  });
+}
+
+function endProcess() {
+  clearInterval(interval);
+  process.stdout.write("\n\nDone!\n");
+  process.exit(0);
+}
+
+function reportError(error) {
+  clearInterval(interval);
+
+  if (error) {
+    process.stdout.write("\n\n");
+    addXMark(() => process.stderr.write(chalk.red(` ${error}\n`)));
+    process.exit(1);
+  }
+}
+
+(async () => {
+  const repoRemoved = await cleanCurrentRepository();
+
+  // Take the required Node and NPM version from package.json
+  const {
+    engines: { node, npm },
+  } = npmConfig;
+
+  const requiredNodeVersion = node.match(/([0-9.]+)/g)[0];
+  await checkNodeVersion(requiredNodeVersion).catch((reason) =>
+    reportError(reason)
+  );
+
+  const requiredNpmVersion = npm.match(/([0-9.]+)/g)[0];
+  await checkNpmVersion(requiredNpmVersion).catch((reason) =>
+    reportError(reason)
+  );
+
+  await installPackages().catch((reason) => reportError(reason));
+  await deleteFileInCurrentDir("setup.js").catch((reason) =>
+    reportError(reason)
+  );
+
+  if (repoRemoved) {
+    process.stdout.write("\n");
+    // interval = animateProgress("Initialising new repository");
+    process.stdout.write("Initialising new repository");
+
+    try {
+      await initGitRepository();
+      await addToGitRepository();
+      await commitToGitRepository();
+    } catch (err) {
+      reportError(err);
+    }
+
+    // addCheckMark();
+    clearInterval(interval);
+  }
+
+  endProcess();
+})();
--- readme.md
+++ readme.md
@@ -1,8 +1,17 @@
 # Basic React Boilerplate
 
 ## Features
+
 React v17<br>
 Webpack 5<br>
 SCSS<br>
 Jest/React Testing Library<br>
-.env files<br>
+Env Files<br>
+PropTypes<br>
+
+## Commands
+
+Run `npm i` to install all dependencies<br>
+Run `npm run dev` to run app in development mode<br>
+Run `npm run start` to serve files to `/public` folder<br>
+Run `npm run test` to run tests<br>
