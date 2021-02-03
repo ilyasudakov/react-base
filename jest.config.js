d18735c630205da600c622e0dc83e7bb8f7bd8db
ilyasudakov <ilyasudakov@inbox.ru>
2021-02-03 10:32:20 +0300

    add jsx files to ESLint

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
+  roots: ['<rootDir>/src'],
   transform: {
-    "\\.(js|jsx)?$": "babel-jest",
+    '\\.(js|jsx)?$': 'babel-jest',
   },
   //   testMatch: ["<rootDir>/src/**/>(*.)test.{js, ts, jsx, tsx}"],
-  moduleFileExtensions: ["js", "jsx", "json", "node"],
-  testPathIgnorePatterns: ["/node_modules/", "/public/"],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/public/'],
   setupFilesAfterEnv: [
-    "./src/setupTests.js",
-    "@testing-library/jest-dom/extend-expect",
+    './src/setupTests.js',
+    '@testing-library/jest-dom/extend-expect',
   ],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const webpack = require("webpack");
-const path = require("path");
-require("dotenv").config();
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const isDevelopment = process.env.NODE_ENV === "development";
+const webpack = require('webpack');
+const path = require('path');
+require('dotenv').config();
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "public"),
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public'),
   },
   module: {
     rules: [
       {
-        loader: "babel-loader",
+        loader: 'babel-loader',
         test: /\.jsx?$/,
         exclude: /node_modules/,
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
         test: /\.(s*)css$/,
         use: [
-          isDevelopment ? MiniCssExtractPlugin.loader : "style-loader",
-          "css-loader",
-          "sass-loader",
+          isDevelopment ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'sass-loader',
         ],
       },
     ],
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: path.resolve(__dirname, "./public"),
+    contentBase: path.resolve(__dirname, './public'),
     open: true,
     compress: true,
     hot: true,
@@ -41,11 +41,11 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "style.css",
+      filename: 'style.css',
     }),
     new webpack.HotModuleReplacementPlugin(),
   ],
   resolve: {
-    extensions: [".js", ".scss", "jsx"],
+    extensions: ['.js', '.scss', 'jsx'],
   },
 };
